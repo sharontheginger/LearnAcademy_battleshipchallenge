8cc9dda3a127fca204115dbbde5cc1a300813d45
sharontheginger <32782067+sharontheginger@users.noreply.github.com>
2018-01-25 15:42:36 -0800

    Add files via upload

--- src/components/Board.js
+++ src/components/Board.js
@@ -9,25 +9,27 @@ const HIT = 2
 const MISS = 3
 const SHIPSIZE = [5,4,3,2,1];
 
+
 export default class Board extends Component {
 
     constructor(props) {
         super(props)
 
         this.state = {
-            torpedoTotalCount: 50,
+            torpedoTotalCount: 9,
             torpedoesFired: 0,
             torpedoesHit: 0,
             differentShips: 0,
+            alreadyClicked:0,
             board: this.setupBoard()
-        }
-
-        for (var i=0; i < 5; i++) {
-            this.placeShip()
         }
     }
 
-
+    componentWillMount() {
+        for (let i = 0; i < 5; i++) {
+            this.placeShip(3)
+        }
+    }
 
     setupBoard() {
         let board = []
@@ -38,50 +40,66 @@ export default class Board extends Component {
                 board[row][col] = EMPTY
             }
         }
+
         return board
     }
 
-    placeShip() {
+    placeShip(size) {
+        const copyOfBoard = this.state.board
+
         // find random cell
-        var row = Math.floor(Math.random()*10);
-        var col = Math.floor(Math.random()*10);
-        // var maxCol = 10 - SHIPSIZE[ship];
-        // var col = Math.floor(Math.random()*maxCol);
-        // var row = ship;
+        var row = Math.floor(Math.random()*10)
+        var col = Math.floor(Math.random()*10)
 
-        // console.log(this.state.board[row][col])
-
-        // add cells for each multicell ship
-        for (var i = 0; i < 3; i++){
-            //if the random cell is empty, put a ship there
-                if (this.state.board[row][col+i] === EMPTY){
-                    // TODO use setState function instead of directly altering state
-                    // TODO make sure that the ship can't go over the side of the board
-                    this.state.board[row][col+i] = SHIP;
-                    // console.log(this.state.board[row][col+shipSquare])
-                    console.log(row + "_" + col)
-                } else {
-                    // if the cell was not empty, try again
-                    // TODO need to find a way to control this -- is currently running too many times
-                    this.placeShip()
-                }
+        // check if we can place the ship -- if true then place the ship
+        if(this.isEmpty(copyOfBoard, row, col, size) === true) {
+            for(let i = 0; i < size; i++) {
+                copyOfBoard[row][col+i] = SHIP
             }
-        }
-
-    handleClick(row, col) {
-        const { board } = this.state
-        if (board[row][col] === SHIP) {
-            this.setState({torpedoesHit: this.state.torpedoesHit + 1})
-            board[row][col] = HIT
-        } else if (board[row][col] === EMPTY) {
-            board[row][col] = MISS
+        } else {
+            this.placeShip(size)
         }
 
         this.setState({
-            board: board,
-            torpedoesFired: this.state.torpedoesFired + 1,
-            torpedoTotalCount: this.state.torpedoTotalCount - 1
+            board: copyOfBoard
         })
+    }
+
+    isEmpty(board, row, col, size) {
+        for(let i = 0; i < size; i++) {
+            // console.log("row:", row, "column:", col, "ship space:", i, " = ", col+i)
+
+            if (board[row][col+i] === SHIP || board[row][col+i] === undefined) {
+                return false
+            }
+        }
+
+        return true
+    }
+
+    handleClick(row, col) {
+        const { board } = this.state
+
+        if (this.state.torpedoesFired >= 9){
+            alert("Game Over!")
+        } else if (board[row][col] === SHIP) {
+            this.setState({
+                board: board,
+                torpedoesHit: this.state.torpedoesHit + 1,
+                torpedoesFired: this.state.torpedoesFired + 1,
+                torpedoTotalCount: this.state.torpedoTotalCount - 1
+            })
+
+            board[row][col] = HIT
+        } else if (board[row][col] === EMPTY) {
+            this.setState({
+                board: board,
+                torpedoesFired: this.state.torpedoesFired + 1,
+                torpedoTotalCount: this.state.torpedoTotalCount - 1})
+            board[row][col] = MISS
+        } else {
+            alert("ALREADY CLICKED!")
+        }
     }
 
     renderRow(row) {
